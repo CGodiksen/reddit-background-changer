3cabaf0a3d6ebf9b20b7d5255d4435c8c5afd16f
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-04-01 11:53:53 +0200

    Implemented a simple AbstractListModel that can be used in a ListView in PyQT.

--- subreddit_model.py
+++ subreddit_model.py
@@ -12,3 +12,25 @@ class SubredditModel(QtCore.QAbstractListModel):
 
     The tuple (/r/aww, "this year", 25) would get the top 25 pictures within the last year from the subreddit /r/aww.
     """
+    def __init__(self, *args, subreddits=None, **kwargs):
+        super(SubredditModel, self).__init__()
+        # The internal storage that will store configuration tuples.
+        self.subreddits = subreddits or []
+
+    def data(self, QModelIndex, role=None):
+        """
+        Returns the data stored under the given role for the item referred to by the index.
+        :param QModelIndex: The specific index of the model that we wish to extract data for.
+        :param role: The specific data that we wish to extract.
+        :return: The name of the subreddit if the role is DisplayRole.
+        """
+        if role == Qt.DisplayRole:
+            name, _, _ = self.subreddits[QModelIndex.row()]
+            return name
+
+    def rowCount(self, parent=None, *args, **kwargs):
+        """
+        Simple function that returns the total rowcount of the internal model representation. Since we use a list this
+        is simply the length of the list.
+        """
+        return len(self.subreddits)
