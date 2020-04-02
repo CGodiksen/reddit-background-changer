ad03da5db37b602844d71381efa1d489542f34e4
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-04-02 23:28:41 +0200

    Added initial code to access reddit api. Also changed some minor stuff in the UI.
    Notice that from here on out you need a config file to run the subreddit model file. This is not included on github due information about the api connection that needs to be kept secret.

--- subreddit_model.py
+++ subreddit_model.py
@@ -1,5 +1,8 @@
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtCore import Qt
+import praw
+import json
+import pprint
 
 
 class SubredditModel(QtCore.QAbstractListModel):
@@ -12,6 +15,7 @@ class SubredditModel(QtCore.QAbstractListModel):
 
     The tuple (/r/aww, "this year", 25) would get the top 25 pictures within the last year from the subreddit /r/aww.
     """
+
     def __init__(self, *args, subreddits=None, **kwargs):
         super(SubredditModel, self).__init__()
         # The internal storage that will store configuration tuples.
@@ -26,7 +30,7 @@ class SubredditModel(QtCore.QAbstractListModel):
         """
         if role == Qt.DisplayRole:
             name, _, _ = self.subreddits[QModelIndex.row()]
-            return name
+            return "/r/" + name
 
     def rowCount(self, parent=None, *args, **kwargs):
         """
@@ -34,3 +38,40 @@ class SubredditModel(QtCore.QAbstractListModel):
         is simply the length of the list.
         """
         return len(self.subreddits)
+
+    def get_images(self, subreddit, save_path):
+        """
+        Uses PRAW to get the images from reddit that are described by the specific subreddit configuration. Once gotten
+        the function saves them to the specified folder.
+        :param save_path: The path to the folder in which we save the images.
+        :param subreddit: The subreddit configuration that describes the subreddit we should search, the time limit
+        the search should be within and the amount of images we should find.
+        :return: None
+        """
+        # Getting the secret information for the reddit application from the config file.
+        with open("config.json", "r") as config_file:
+            config = json.load(config_file)
+
+        reddit = praw.Reddit(client_id=config["client_id"], client_secret=config["client_secret"],
+                             user_agent=config["user_agent"])
+
+        subreddit = reddit.subreddit('aww')
+
+        for submission in subreddit.hot(limit=10):
+            if submission.is_reddit_media_domain and submission.is_video is False:
+                print(submission.preview["images"][0]["source"])
+                print(submission.preview["images"][0]["source"]["width"])
+                print(submission.preview["images"][0]["source"]["height"])
+                print(submission.preview["images"][0]["source"]["url"])
+
+    def get_all_images(self, save_path):
+        """
+        Finds the specified images for every subreddit configuration that is currently in the internal list model.
+        :param save_path: The path to the folder in which we save the images.
+        :return: None
+        """
+
+
+testing = SubredditModel()
+
+testing.get_images(None, None)
