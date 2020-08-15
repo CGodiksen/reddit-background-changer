bd98329e5df087f1bfecfa316ede54b000619530
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-08-15 16:44:16 +0200

    Added a dialog window used to change the settings of the application. Also added a class that supports the functionality behind this new dialog window.

---
+++ settings.py
@@ -0,0 +1,46 @@
+import json
+import os
+
+
+class Settings:
+    def __init__(self):
+        # If the settings file does not already exist then initialize the settings file.
+        if "settings.json" not in os.listdir("resources"):
+            self.initialize_file()
+
+        self.client_id = ""
+        self.client_secret = ""
+        self.user_agent = ""
+        self.change_frequency = 0
+        self.blacklist = []
+
+        self.load_settings()
+
+    def load_settings(self):
+        """Loading the current settings from the settings file."""
+        with open("resources/settings.json", "r") as settings_file:
+            settings = json.load(settings_file)
+            self.client_id = settings["client_id"]
+            self.client_secret = settings["client_secret"]
+            self.user_agent = settings["user_agent"]
+            self.change_frequency = settings["change_frequency"]
+            self.blacklist = settings["blacklist"]
+
+    def save_settings(self):
+        """Saving the current settings to the the settings file"""
+        with open("resources/settings.json", "w") as settings_file:
+            json.dump({"client_id": self.client_id,
+                       "client_secret": self.client_secret,
+                       "user_agent": self.user_agent,
+                       "change_frequency": self.change_frequency,
+                       "blacklist": self.blacklist}, settings_file)
+
+    @staticmethod
+    def initialize_file():
+        """Initializes the settings file."""
+        with open("resources/settings.json", "w+") as settings_file:
+            json.dump({"client_id": "",
+                       "client_secret": "",
+                       "username": "",
+                       "change_frequency": 30,
+                       "blacklist": []}, settings_file)
---
+++ settings_dialog.py
@@ -0,0 +1,40 @@
+from settings import Settings
+
+from PyQt5 import QtWidgets, uic
+
+
+class SettingsDialog(QtWidgets.QDialog):
+    def __init__(self, *args, **kwargs):
+        super(SettingsDialog, self).__init__(*args, **kwargs)
+
+        # Load the UI Page.
+        uic.loadUi("resources/settingsdialog.ui", self)
+
+        # Loading the settings and writing them to their respective line edits.
+        self.settings = Settings()
+        self.clientIDLineEdit.setText(self.settings.client_id)
+        self.clientSecretLineEdit.setText(self.settings.client_secret)
+        self.userAgentLineEdit.setText(self.settings.user_agent)
+        self.changeFrequencySpinBox.setValue(self.settings.change_frequency)
+
+        # Saving the text in the line edits to the settings file when the user presses the "OK" button.
+        self.accepted.connect(self.ok)
+
+        # Reverting any changes if the user presses the "Cancel" button.
+        self.rejected.connect(self.cancel)
+
+    def ok(self):
+        """Saving the text in the line edits to the settings file."""
+        self.settings.client_id = self.clientIDLineEdit.text()
+        self.settings.client_secret = self.clientSecretLineEdit.text()
+        self.settings.user_agent = self.userAgentLineEdit.text()
+        self.settings.change_frequency = self.changeFrequencySpinBox.text()
+
+        self.settings.save_settings()
+
+    def cancel(self):
+        """Reverting the text in the line edits back to the initial text."""
+        self.clientIDLineEdit.setText(self.settings.client_id)
+        self.clientSecretLineEdit.setText(self.settings.client_secret)
+        self.usernameLineEdit.setText(self.settings.username)
+        self.changeFrequencySpinBox.setValue(self.settings.change_frequency)
